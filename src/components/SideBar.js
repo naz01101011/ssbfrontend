8a84d9bf8f1ebb74a095ea2fb845dbe78293c571
Alex <alex@pop-os.localdomain>
2020-08-20 22:45:32 +0300

    Fixed newslist

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -3,12 +3,21 @@ import AdsRectangle from '../components/AdsRectangle';
 import NewsList from '../components/NewsList';
 
 const SideBar = (props) => {
-    return (
-        <div>
-            <AdsRectangle />
-            <NewsList list={props.list} images='false'/>
-        </div>
-    )
+    if (props.list) {
+        return (
+            <div>
+                <AdsRectangle />
+                <NewsList list={props.list} images='false'/>
+            </div>
+        )
+    } else {
+        return (
+            <div>
+                <AdsRectangle />
+            </div>
+        )
+    }
+    
 }
 
 export default SideBar;
