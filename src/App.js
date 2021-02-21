876be47aa38005ad9bfb11b96d2355d92a6227aa
Alex <alex@pop-os.localdomain>
2021-02-21 15:42:49 +0200

    fixed archive, styled GDPR notification and added trafic.ro code

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React, {useEffect, useState} from 'react';
-import {BrowserRouter as Router, Switch, Route, } from 'react-router-dom';
-import AppHeader from './components/AppHeader';
-import Home from './pages/Home';
-import Stiri from './pages/Stiri';
-import Contact from './pages/Contact'
-import Privacy from './pages/Privacy'
-import Article from './pages/Article';
-import Search from './pages/Search';
-import AdsContext from './context/AdsContext';
-import Client from './components/Client'
-import AppFooter from './components/AppFooter';
-
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import AppHeader from "./components/AppHeader";
+import Home from "./pages/Home";
+import Stiri from "./pages/Stiri";
+import Contact from "./pages/Contact";
+import Privacy from "./pages/Privacy";
+import Article from "./pages/Article";
+import Search from "./pages/Search";
+import AdsContext from "./context/AdsContext";
+import Client from "./components/Client";
+import AppFooter from "./components/AppFooter";
+import CookieConsent from "react-cookie-consent";
+import { Link } from "react-router-dom";
 
 function App() {
   const adsquery = '*[_type == "banner"] | order(publishedAt desc)';
@@ -18,15 +19,14 @@ function App() {
   const [ads, setAds] = useState({});
 
   const fetchAds = () => {
-    Client.fetch(adsquery)
-        .then(res => setAds(res))
-  }
+    Client.fetch(adsquery).then((res) => setAds(res));
+  };
 
   // console.log(ads)
 
   useEffect(() => {
-      fetchAds();
-  }, [])
+    fetchAds();
+  }, []);
 
   return (
     <AdsContext.Provider value={ads}>
@@ -41,6 +41,25 @@ function App() {
           <Route path="/stiri" component={Stiri} />
         </Switch>
         <AppFooter />
+        <CookieConsent
+          location="bottom"
+          buttonText="OK"
+          style={{ color: "#888", background: "#FFF", border: "2px solid #888", borderRadius: "10px", margin: "10px", width: "auto", right: "0", left: "auto" }}
+          buttonStyle={{
+            background: "#f4511e",
+            color: "#fff",
+            fontSize: "13px"
+          }}
+          expires={1}
+          debug={true}
+        >
+          Acest site foloseste cookies pentru a imbunatatii experienta
+          cititorilor. Citeste{" "}
+          <Link className="orange-text text-darken-4" to="/confidentialitate">
+            Politica de Confidențialitate
+          </Link>{" "}
+          pentru a intelege cum folosim datele personale.
+        </CookieConsent>
       </Router>
     </AdsContext.Provider>
   );
--- src/pages/Stiri.js
+++ src/pages/Stiri.js
@@ -1,60 +1,67 @@
-import React, {useEffect, useState} from 'react';
-import Client from '../components/Client';
-import NewsList from '../components/NewsList';
-import SideBar from '../components/SideBar';
-import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css'
+import React, { useEffect, useState } from "react";
+import Client from "../components/Client";
+import NewsList from "../components/NewsList";
+import SideBar from "../components/SideBar";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
 
 const Stiri = () => {
+  const [archive, setArchive] = useState([]);
+  const [loaded, setLoaded] = useState(false);
+  const [value, setValue] = useState(new Date());
+  const [theDate, setTheDate] = useState();
 
-    const [archive, setArchive] = useState([]);
-    const [loaded, setLoaded] = useState(false);
-    const [value, setValue] = useState(new Date());
-    const [theDate, setTheDate] = useState();
+  useEffect(() => {
+    setTheDate(
+        value.getFullYear() + '-' + ('0' + (value.getMonth()+1)).slice(-2) + '-' + ('0' + value.getDate()).slice(-2)
+    );
+    const query =
+      '*[_type == "post" && publishedAt >= "' +
+      theDate +
+      'T00:00:00.000Z" && publishedAt <= "' +
+      theDate +
+      'T23:59:59.000Z"] | order(publishedAt desc) { _id, title, publishedAt, "categ": categories[0]->title, "authorName": authors[0].author->name, "slug": slug.current}';
+    const fetchData = () => {
+      Client.fetch(query).then((res) => {
+        setArchive(res);
+        setLoaded(true);
+      });
+    };
+    fetchData();
+    console.log(theDate);
+  }, [value, theDate]);
 
-    useEffect(() => {
-        setTheDate(value.getFullYear() + "-" + (value.getMonth() + 1) + "-" + value.getDate());
-        const query = '*[_type == "post" && publishedAt >= "' + theDate + 'T00:00:00.000Z" && publishedAt <= "' + theDate + 'T23:59:59.000Z"] | order(publishedAt desc) { _id, title, publishedAt, "categ": categories[0]->title, "authorName": authors[0].author->name, "slug": slug.current}';
-        const fetchData = () => {
-            Client.fetch(query)
-                .then(res => {
-                    setArchive(res)
-                    setLoaded(true)
-                })
-        }
-        fetchData();
-        console.log(theDate);
-    }, [value, theDate])
-
-
-    return (
-        <main className='container'>
-            {loaded ? (
-            <div>
-                <div className='row section'>
-                    <div className='col s12 m8'>
-                      <h5>Știri publicate în data de {value.getDate()}.{value.getMonth() + 1}.{value.getFullYear()}</h5>
-                      <NewsList list={archive} images='false'/>
-                    </div>
-                    <div className='col s12 m4'>
-                      <h5>Filtrează știrile după dată</h5>
-                      <Calendar
-                        onChange={setValue}
-                        value={value}
-                        calendarType="ISO 8601"
-                        locale="ro-RO"
-                      />
-                      <SideBar />
-                    </div>
-                </div>
+  return (
+    <main className="container">
+      {loaded ? (
+        <div>
+          <div className="row section">
+            <div className="col s12 m8">
+              <h4>
+                Știri publicate în data de {('0' + value.getDate()).slice(-2)}.
+                {('0' + (value.getMonth()+1)).slice(-2)}.{value.getFullYear()}
+              </h4>
+              <NewsList list={archive} images="false" />
             </div>
-            ) : (
-                <div>
-                    <h4 className='center'>Aduc din arhivă...</h4>
-                </div>
-            )}
-        </main>
-    )
-}
+            <div className="col s12 m4">
+              <h5>Filtrează știrile după dată</h5>
+              <Calendar
+                onChange={setValue}
+                value={value}
+                calendarType="ISO 8601"
+                locale="ro-RO"
+              />
+              <SideBar />
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <h4 className="center">Aduc din arhivă...</h4>
+        </div>
+      )}
+    </main>
+  );
+};
 
 export default Stiri;
