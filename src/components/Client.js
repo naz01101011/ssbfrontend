1df655c3f511a4e4d5671146ef1b4fa8f2e572f7
Alex Cazan <alex@switchcase.dev>
2021-03-29 16:26:29 +0300

    switched to cdn

--- src/components/Client.js
+++ src/components/Client.js
@@ -4,7 +4,7 @@ const Client = sanityClient({
     projectId: 'w58sh5v7',
     dataset: 'production',
     // token: 'skDigpIvznVq9up1wcFDcXXRHo8pmGTDa440hwkbxDA7iSo7rhsQdWu1aitWkKYuOOxUPaJ6rZTB0GrzEA6dA0gq5PfiLzUSlnTydsLIueVKxYm5IXXZqMrDzttVNgOwyo4QDBd6f0HhWIoflGpWyPFbkWNxw2PgFl48T5SmtdCeGoXELieK',
-    useCdn: false
+    useCdn: true
 })
 
 export default Client;
