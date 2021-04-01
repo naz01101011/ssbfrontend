39f3655e677a7a6993899febc4480007ab91b728
Alex Cazan <alex@switchcase.dev>
2021-04-01 15:53:30 +0300

    added first adsense

--- src/components/AdsRectangle.js
+++ src/components/AdsRectangle.js
@@ -1,6 +1,7 @@
-import React, {useContext} from 'react';
+import React, { useContext } from 'react';
 import AdsContext from '../context/AdsContext';
-import urlFor from './ImgBuilder'
+import urlFor from './ImgBuilder';
+import AdSense from 'react-adsense';
 
 const Rectangle = () => {
     let ads = useContext(AdsContext)
@@ -22,20 +23,27 @@ const Rectangle = () => {
                                 <img className='responsive-img' src={urlFor(ad.bannerImg).width(300).url()} id={ad._id} alt='tall' />
                             </a>
                         </div>
-                    )    
+                    )
                 })}
-                
+
             </div>
         );
     } else {
         return (
             <div className='center grey lighten-4 adcontainer'>
                 {/* <span className='adlabel left-align'>Google Ads</span> */}
+                <AdSense.Google 
+                    client='ca-pub-2723789094037700'
+                    slot='4430780933'
+                    format='auto'
+                    style={{ display: 'block' }}
+                    responsive='true'
+                />
             </div>
         );
     }
 
-    
+
 }
 
 export default Rectangle;
