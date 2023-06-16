db143739975e0ca95e2fe3156a7a70dac98b518b
Alexandru Cazan <alexandru.cazan@logiccloud.de>
2023-06-16 13:51:20 +0300

    move fits to header

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -7,26 +7,26 @@ import AdsLeaderboard from '../components/AdsLeaderboard';
 const AppHeader = (props) => {
     return (
         <header>
-            <div className='topflag'>
-              <a href='https://www.sibfest.ro' target={'_blank'} rel='noopener noreferrer'> <img src='./fits.png' width={1170} height={90} alt="FITS 2023" /></a>
-            </div>
             <nav>
                 <div className="nav-wrapper grey lighten-4">
-                    <div className='container'>
-                        <Link className="brand-logo deep-orange-text text-darken-1" to='/' >știri<span style={{color:"grey"}}>de</span>Sibiu<span style={{color:"grey"}}>.ro</span></Link>
-                        <ul id="nav-mobile" className="right hide-on-med-and-down">
-                            <li><SearchBox /></li>
-                            <li>
-                                <Link className='grey-text text-darken-2' to='/' >Acasă</Link>
-                            </li>
-                            <li>
-                                <Link className='grey-text text-darken-2' to='/stiri' >Arhivă</Link>
-                            </li>
-                            <li>
-                                <Link className='grey-text text-darken-2' to='/contact'>Contact</Link>
-                            </li>
-                        </ul>
-                    </div>
+                  <div className='topflag'>
+                    <a href='https://www.sibfest.ro' target={'_blank'} rel='noopener noreferrer'> <img src='./fits.png' width={1170} height={90} alt="FITS 2023" /></a>
+                  </div>
+                  <div className='container'>
+                      <Link className="brand-logo deep-orange-text text-darken-1" to='/' >știri<span style={{color:"grey"}}>de</span>Sibiu<span style={{color:"grey"}}>.ro</span></Link>
+                      <ul id="nav-mobile" className="right hide-on-med-and-down">
+                          <li><SearchBox /></li>
+                          <li>
+                              <Link className='grey-text text-darken-2' to='/' >Acasă</Link>
+                          </li>
+                          <li>
+                              <Link className='grey-text text-darken-2' to='/stiri' >Arhivă</Link>
+                          </li>
+                          <li>
+                              <Link className='grey-text text-darken-2' to='/contact'>Contact</Link>
+                          </li>
+                      </ul>
+                  </div>
                 </div>
             </nav>
             <AdsLeaderboard position='large-top-leaderboard' googleads='false'/>
